3f8e72eb01dd6c977d6641303cc086ea04c9edd3
x2RunE <zeenogenesis.space@proton.me>
2025-12-23 02:00:32 +0700

    Update pro.lua

--- pro.lua
+++ pro.lua
@@ -1,1 +1,6 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ef1142e6e0c5dc2c7feaa45ee24f1068.lua"))()
+local executor = getexecutorname() or identifyexecutor()
+if executor:find('Seliware') or executor:find('Potassium') then
+  loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/430615b200c84fe7"))()
+else
+  loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ef1142e6e0c5dc2c7feaa45ee24f1068.lua"))()
+end
