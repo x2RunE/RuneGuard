2ef3bdb2762d4e92f667c78984c0feaadfc88a1d
x2RunE <zeenogenesis.space@proton.me>
2026-01-06 11:12:28 +0700

    Update pro.lua

--- pro.lua
+++ pro.lua
@@ -1,5 +1,5 @@
 local executor = getexecutorname() or identifyexecutor()
-if executor:find('Seliware') then
+if executor:find('Seliware') or executor:find('Delta') then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/x2RunE/RuneGuard/refs/heads/main/script.lua"))()
 else
   loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ef1142e6e0c5dc2c7feaa45ee24f1068.lua"))()
