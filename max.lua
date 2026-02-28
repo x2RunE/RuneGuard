7f27978262cb1ffe5caa55370e17ed0a1747b929
x2RunE <zeenogenesis.space@proton.me>
2026-02-28 18:08:06 +0700

    Update max.lua

--- max.lua
+++ max.lua
@@ -1,21 +1,3 @@
 repeat wait() until game:IsLoaded()
-pcall(function()
-    pcall(function()
-        pcall(function()
-            if getconnections then
-                for _, v in pairs(getgc(true)) do
-                    if type(v) == "function" then
-                        local info = debug.getinfo(v)
-                        local name = info.name:lower()
-                        if name == "detected" or name == 'crash' then
-                            hookfunction(v, function() end)
-                        end
-                     end
-                 end
-                 for i,v in next,getconnections(game:GetService('ScriptContext').Error) do v:Disconnect() end
-            end
-        end)
-    end)
-    task.wait(1.25)
-end)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/x2RunE/QuynhLaSo1/refs/heads/main/data/gpo-bypass.lua"))()
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/3437e5df438f126562c22e3e31ad895b.lua"))()
