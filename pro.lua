7beae1bf6bcf8001854f4c11b4428674f141af06
x2RunE <zeenogenesis.space@proton.me>
2026-03-06 00:42:31 +0700

    Update pro.lua

--- pro.lua
+++ pro.lua
@@ -1,21 +1,2 @@
-local ServiceNames = {"ReplicatedStorage","TweenService", "Players", "Workspace","RunService","UserInputService"}
-local Services = {}
-for _, Service in ipairs(ServiceNames) do 
-    Services[Service] = cloneref(game:GetService(Service))
-end 
-local rs = Services.ReplicatedStorage
-local plrs = Services.Players
-local plr = plrs.LocalPlayer
-repeat
-    task.wait(1.25)
-until rs and plr
-pcall(function()
-    pcall(function()
-        local crabRemote = rs:FindFirstChild("Crab_Strangler")
-        if crabRemote then
-            crabRemote:Destroy()
-        end
-    end)
-    task.wait(1.25)
-end)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/x2RunE/QuynhLaSo1/refs/heads/main/data/gpo-bypass.lua"))()
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ef1142e6e0c5dc2c7feaa45ee24f1068.lua"))()
