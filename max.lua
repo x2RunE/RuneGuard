dac4ace627917a492d8fd79f3cece4a8fb0200df
x2RunE <zeenogenesis.space@proton.me>
2026-03-06 00:43:15 +0700

    Update max.lua

--- max.lua
+++ max.lua
@@ -1,3 +1,1 @@
-repeat wait() until game:IsLoaded()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/x2RunE/QuynhLaSo1/refs/heads/main/data/gpo-bypass.lua"))()
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/3437e5df438f126562c22e3e31ad895b.lua"))()
+game.Players.LocalPlayer:Kick('Outdated')
